f376ab0d3e5f7d6dd750b6947cc2ae8e8349b1fc
ThiagoEXE <your-email-addressthiagob.oliveira@ucsal.edu.br>
2024-02-29 21:41:19 -0300

    Adição de loop com for

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,11 +7,15 @@ numero_secreto = 42
 total_tentativas = 3
 rodada = 1
 
-while(rodada <= total_tentativas):
-    print("Tentativa:", rodada, "de", total_tentativas)
-    chute_str = input("Digite o seu número: ")
+for rodada in range(1, total_tentativas + 1):
+    print("Tentativa {} de {}" .format(rodada, total_tentativas))
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou " , chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um valor entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -19,6 +23,7 @@ while(rodada <= total_tentativas):
 
     if(acertou):
         print("Parabéns! Você acertou!")
+        break
     else:
         if(maior):
             print("O seu chute foi maior do que o número secreto!")
